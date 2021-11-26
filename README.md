2880fa42ed13fa6e3dc9d94ae1e29e255cb90273
imurlobster <alexander2720@163.com>
2021-11-26 16:42:45 +0800

    chore:lua learning step one

---
+++ README.md
@@ -0,0 +1,14 @@
+**The practice for lua script**
+
+*[菜鸟教程 Runoob Tutorial](https://www.runoob.com/lua/lua-tutorial.html)*
+*[官方文档 Lua Official Documentation]*(http://www.lua.org/docs.html)
+
+
+* lua simple installation
+  * via homebrew on macOS
+  * Guide/Suggestion for a novice in Lua
+  * 1.open your terminal or iTrem2 shell bash prompt
+  * type: brew install lua
+  * if the result is successful, take the following step 2
+  * 2.lua -i
+  * Lua 5.4.3  Copyright (C) 1994-2021 Lua.org, PUC-Rio
---
+++ basic-grammar/hello.lua
@@ -0,0 +1,48 @@
+#!/usr/local/bin/lua
+
+-- simple introduction
+
+-- Catalog
+--[[
+  hello world
+  comment style (now you are seeing it)
+  initialize variables
+  inexistence (equal to null in JavaScript)
+--]]
+
+
+-- 1.hello world first thing
+print('hello world')
+
+
+-- 2.comment style
+-- for single-line comment
+-- just like this as you see
+-- as for mutiple-line comment
+--[[
+  That's this
+  It's so cute and cool
+  Ah, I love lua
+  beautiful && gorgeous
+--]]
+
+
+-- 3.initialize variables
+--[[
+  在默认情况下，变量总是认为是全局的。
+  全局变量不需要声明，给一个变量赋值后即创建了这个全局变量，访问一个没有初始化的全局变量也不会出错，只不过得到的结果是：nil。
+--]]
+luaVar = 'I am a global variable';
+
+-- the variable's name must avoid the keywords of lua
+-- and	break	do	else
+-- elseif	end	false	for
+-- function	if	in	local
+-- nil	not	or	repeat
+-- return	then	true	until
+-- while	goto
+
+
+-- 4.inexistence (equal to null in JavaScript)
+inexistenceVar = nil
+print('It will show nothing', inexistenceVar, 'you find no letter in the middle of sentence')
