5759189ce68da6f5a2863765c8dbc882f2adf8f6
imurlobster <alexander2720@163.com>
2021-11-30 16:32:54 +0800

    feat: 添加Lua的数据类型的case

---
+++ datatypes/datatypes.lua
@@ -0,0 +1,38 @@
+print(type(a))
+-- nil 未赋值的变量值为nil
+
+
+table1 = {
+  key1 = "val1",
+  key2 = "val2",
+  "value3"
+}
+
+for k, v in pairs(table1) do
+  print(k .. "--" .. v)
+end
+
+table1.key2 = "value2"
+table1[1] = "valueChange3"
+table1[0] = "zero"
+
+for k, v in pairs(table1) do
+  print(k .. "==" .. v)
+end
+
+-- weird, I got confused
+-- todo
+-- Result:
+--[[
+    nil
+
+    1--value3
+    key1--val1
+    key2--val2
+
+    1==valueChange3
+    key1==val1
+    0==zero
+    key2==value2
+--]]
+
